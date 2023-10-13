0173be1bf979628275b6878ca20bb270ce884904
abdullahalwafi1 <abdullahalwafi16@gmail.com>
2023-10-13 21:51:27 +0700

    upload tugas pertemuan 3 : OOP CRUD animal

---
+++ task/Animals.php
@@ -0,0 +1,59 @@
+<?php
+class Animal
+{
+    public $data = array();
+
+    public function __construct()
+    {
+        $this->data =
+            [
+                [
+                    'name' => 'Ayam',
+                ],
+                [
+                    'name' => 'Ikan',
+                ],
+            ];
+    }
+
+    public function index()
+    {
+        foreach ($this->data as $key => $value) {
+            echo '- ' . $value['name'] . '<br>';
+        }
+    }
+
+    public function store($name)
+    {
+        array_push($this->data, ["name" => $name]);
+    }
+
+    public function update($index, $name)
+    {
+        $this->data[$index]['name'] = $name;
+    }
+    public function destroy($index)
+    {
+        unset($this->data[$index]);
+    }
+}
+
+$animal = new Animal();
+echo 'Index - Menampilkan Seluruh Hewan <br>';
+$animal->index();
+echo '<br>';
+
+echo 'Store - Menambahkan Hewan Baru (burung) <br>';
+$animal->store('Burung');
+$animal->index();
+echo '<br>';
+
+echo 'Update - Mengedit hewan <br>';
+$animal->update(0, "Kucing Anggora");
+$animal->index();
+echo '<br>';
+
+echo 'Delete - Menghapus hewan <br>';
+$animal->destroy(1);
+$animal->index();
+echo '<br>';
